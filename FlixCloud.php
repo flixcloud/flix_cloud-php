bc40ba5baf0aff37b7b0f9d1eeb79f672fafadca
Steve Heffernan <steve@sevenwire.com>
2009-04-17 21:07:32 -0500

    Switched documentation to README file. Updated notification object to work with new XML.

--- FlixCloud.php
+++ FlixCloud.php
@@ -1,133 +1,11 @@
-<?php
+<?php 
 /*
+
   FlixCloud API PHP Library
-
-  Author:  Steve Heffernan <steve@sevenwire.com>
   Version: 1.0.6
-  Date:    04-01-2009
-
-  For more details on the FlixCloud API requirements visit
-  http://flixcloud.com/api/
-
-  There are two main functions: Job Requests, and Notification Handling.
-
-  ////////////////////////////////////////////////////////////////////////////
-
-  TRANSCODING JOB REQUEST
-  The FlixCloudJob object uses cURL to send an XML formatted job request to
-  https://www.flixcloud.com/jobs. It then uses the SimpleXMLElement library
-  to parse any returned XML.
-
-  ** API key is required and can be found at https://flixcloud.com/settings **
-  ** Recipe ID is required and can be found at http://flixcloud.com/overviews/recipes **
-
-  EXAMPLE 1 ------------------------------------------------------------------
-
-  require("FlixCloud.php");
-
-  // First parameter is API key, second is Recipe ID
-  $fcj = new FlixCloudJob("2j1l:kd3add:0:ivzf:2e1y", "99");
-  $fcj->set_input("http://www.example.com/videos/input.mpg"); // Add username/password if needed
-  $fcj->set_output("ftp://www.example.com/httpdocs/videos/output.flv", "username", "password");
-  $fcj->set_watermark("http://www.example.com/videos/watermark.png"); // Add username/password if needed
-
-  if($fcj->send()) {
-    // Job was successfully requested. Store job ID if needed.
-    echo "Success. Job ID is ".$fcj->id." Submitted at ".$fcj->initialized_job_at;
-  } else {
-    // Do something with the errors
-    foreach($fcj->errors as $error) {
-      echo $error;
-    }
-  }
-
-
-  EXAMPLE 2 (using params hash) -----------------------------------------------
-
-  require("FlixCloud.php");
-
-  $fcj = new FlixCloudJob("2j1l:kd3add:0:ivzf:2e1y", array(
-    "recipe_id"       => 99,
-    "input_url"       => "http://www.example.com/videos/input.mpg",
-    // Add input_user, input_password if needed
-    "output_url"      => "sftp://www.example.com/httpdocs/videos/output.flv",
-    "output_user"     => "username",
-    "output_password" => "password",
-    "watermark_url"   => "http://www.example.com/videos/watermark.png",
-    // Add watermark_user, watermark_password if needed
-  ));
-
-  if($fcj->success) {
-    echo "Success. Job ID is ".$fcj->id." Submitted at ".$fcj->initialized_job_at;
-  } else {
-    foreach($fcj->errors as $error) {
-      echo $error;
-    }
-  }
-
-  ////////////////////////////////////////////////////////////////////////////
-
-  NOTIFICATION HANDLING
-  The FlixCloudNotificationHandler class is used to capture and parse XML data sent from
-  FlixCloud when a job has been completed.
-
-  The possible states of the FlixCloudJobNotification object are "successful_job",
-  "cancelled_job", or "failed_job". A callback function allows you to respond accordingly.
-
-  ** Notification URL must be set in https://flixcloud.com/settings **
-  ** A file following the example below should be at that URL **
-
-  EXAMPLE --------------------------------------------------------------------
-
-  require("FlixCloud.php");
-
-  $notification = FlixCloudNotificationHandler.catch_and_parse();
-
-  switch ($notification->state) {
-      case "successful_job":
-        // Do something
-        break;
-      case "cancelled_job":
-        // Do something else
-        break;
-      case "failed_job":
-        // Do something with error message
-        break;
-      default:
-        // Something errored. Bad XML, or lack of SimpleXMLElement parser.
-  }
-
-  ////////////////////////////////////////////////////////////////////////////
-  Suggestions -> steve@sevenwire.com
-
-  Version 1.0.6 - 4/14/09   Cleaned up the documentation a little.
-
-  Version 1.0.5 - 4/6/09    Added options for pointing to the certificate or certificate directory.
-
-  Version 1.0.4 - 4/6/09    Added option to connect insecurely incase SSL cert isn't working.
-                              Like curl -k
-
-  Version 1.0.3 - 4/6/09    Added backward compatability for recipe as second argument
-                            Added Curl error checking
-                            Added more detailed unknown error reporting
-                            Formatted XML a little
-
-  Version 1.0.2 - 4/5/09 -  * Now requiring API key on object creation, and added an optional params hash
-                                for setting file info and sending on creation (example 2). *
-                            * Recipe is no longer second argument *
-                            * Broke up notification handling into 2 objects (see example) *
-                                FlixCloudNotificationHandler => Catch and parse XML
-                                FlixCloudJobNotification => Holds job notification info.
-                            Added escaping of special characters when creating XML tags.
-                            Removed protocol checking for now, because may want to just let the API do that.
-                            On job->send changed status code handling to a switch statement.
-
-  Version 1.0.1 - 4/2/09 -  Fixed acceptable protocols for some of the file types.
-
-  NOTES:
-
-  The windows version of curl will automatically look for a CA certs file named ´curl-ca-bundle.crt´, either in the same directory as curl.exe, or in the Current Working Directory, or in any folder along your PATH
-
+  Author: Steve Heffernan <steve@sevenwire.com>
+  See the README file for info on how to use this library. 
+  
 */
 
 
@@ -166,9 +44,10 @@ class FlixCloudJob {
     $this->api_key = $api_key;
     if (is_array($params)) {
       if($params["recipe_id"]) $this->recipe_id = $params["recipe_id"];
-      if($params["input_url"]) $this->set_input($params["input_url"], $params["input_user"], $params["input_password"]);
-      if($params["output_url"]) $this->set_output($params["output_url"], $params["output_user"], $params["output_password"]);
-      if($params["watermark_url"]) $this->set_watermark($params["watermark_url"], $params["watermark_user"], $params["watermark_password"]);
+      
+      if($params["input_url"]) $this->set_input($params["input_url"], array("user" => $params["input_user"], "password" => $params["input_password"]));
+      if($params["output_url"]) $this->set_output($params["output_url"], array("user" => $params["output_user"], "password" => $params["output_password"]));
+      if($params["watermark_url"]) $this->set_watermark($params["watermark_url"], array("user" => $params["watermark_user"], "password" => $params["watermark_password"]));
 
       if($params["insecure"]) $this->insecure = true;
       if($params["certificate"]) $this->certificate = $params["certificate"];
@@ -180,18 +59,18 @@ class FlixCloudJob {
   }
 
   // Create input file object from URL and option user credentials
-  function set_input($file_url, $user = "", $password = "") {
-    $this->input = new FlixCloudJobInputFile($file_url, $user, $password);
+  function set_input($url, $params = array()) {
+    $this->input = new FlixCloudJobInputFile($url, $params);
   }
 
   // Create output file object from URL and option user credentials
-  function set_output($file_url, $user = "", $password = "") {
-    $this->output = new FlixCloudJobOutputFile($file_url, $user, $password);
+  function set_output($url, $params = array()) {
+    $this->output = new FlixCloudJobOutputFile($url, $params);
   }
 
   // Create watermark file object from URL and option user credentials
-  function set_watermark($file_url, $user = "", $password = "") {
-    $this->watermark = new FlixCloudJobWatermarkFile($file_url, $user, $password);
+  function set_watermark($url, $params = array()) {
+    $this->watermark = new FlixCloudJobWatermarkFile($url, $params);
   }
 
   // Check that all required info is available and valid. Used before sending.
@@ -362,22 +241,28 @@ class FlixCloudJob {
 class FlixCloudJobFile {
 
   var $name = "base"; // Used in errors
-  var $file_url;      // Location of file including tranfer protocol (http, ftp, etc.)
+  var $url;      // Location of file including tranfer protocol (http, ftp, etc.)
   var $user;          // Username
   var $password;      // Password
   var $protocol;      // Set and used in validating.
   var $errors = array();
+  
+  // Info received from notification
+  var $width;     // In pixels
+  var $height;    // In pixels
+  var $size;      // In bytes
+  var $duration;  // In milliseconds
+  var $cost;      // In millicents
 
   // Initialize
-  function FlixCloudJobFile($file_url, $user = "", $password = "") {
-    $this->file_url = trim($file_url);
-    $this->user = trim($user);
-    $this->password = trim($password);
+  function FlixCloudJobFile($url, $params = array()) {
+    $this->url = trim($url);
+    $this->set_attributes($params);
   }
 
   // Validate that all needed data is available.
   function validate() {
-    if( !$this->file_url) $this->errors[] = $this->name." file url required.";
+    if( !$this->url) $this->errors[] = $this->name." file url required.";
     if($this->user && !$this->password) $this->errors[] = $this->name." password needed (user supplied).";
     if($this->password && !$this->user) $this->errors[] = $this->name." user needed (password supplied).";
     // Check that an appropriate protocol is being used.
@@ -399,7 +284,7 @@ class FlixCloudJobFile {
 
   // Check if protocal supplied matches an accepted protocol for the file type
   function check_protocol() {
-    preg_match ("/^[^:]+/i", $this->file_url, $matches);
+    preg_match ("/^[^:]+/i", $this->url, $matches);
     $this->protocol = $matches[0];
     if( !in_array($this->protocol, $this->acceptable_protocols)) {
       return false;
@@ -409,11 +294,19 @@ class FlixCloudJobFile {
 
   // Return a hash of values to be turned into XML later
   function get_data_hash() {
-    $hash = array("url" => $this->file_url);
+    $hash = array("url" => $this->url);
     if($this->user) {
       $hash["parameters"] = array("user" => $this->user, "password" => $this->password);
     }
     return $hash;
+  }
+  
+  function set_attributes($hash) {
+    if(is_array($hash)) {
+      foreach($hash as $key => $value) {
+        $this->$key = $value;
+      }
+    }
   }
 }
 
@@ -446,7 +339,8 @@ class FlixCloudNotificationHandler {
 // Class for catching and parsing XML data sent from FlixCloud when job is finished.
 // Notification URL must be set in https://flixcloud.com/settings
 class FlixCloudJobNotification {
-  var $hash;                   // Original hash
+  
+  var $original_hash;          // From XML object
   var $error_message;          // Error message if there was one.
   var $finished_job_at;        // When the job finished. UTC YYYY-MM-DDTHH:MM:SSZ
   var $id;                     // The job's ID
@@ -455,21 +349,39 @@ class FlixCloudJobNotification {
   var $state;                  // failed_job, cancelled_job, or successful_job
   var $recipe_name;            // Name of recipe used
   var $watermark_file;         // Location of watermark file
-  var $input_media_file;       // Location of input file
-  var $output_media_file;      // Location of output file
+  
+  var $input_media_file;       // DEPRECATED use $fcjn->input->url
+  var $output_media_file;      // DEPRECATED use $fcjn->output->url
 
   function FlixCloudJobNotification($hash) {
-    $this->hash               = $hash;
-    $this->error_message      = trim($hash["error-message"]);
-    $this->finished_job_at    = trim($hash["finished-job-at"]);
-    $this->id                 = trim($hash["id"]);
-    $this->initialized_job_at = trim($hash["initialized-job-at"]);
-    $this->recipe_id          = trim($hash["recipe-id"]);
-    $this->state              = trim($hash["state"]);
-    $this->recipe_name        = trim($hash["recipe-name"]);
-    $this->watermark_file     = trim($hash["watermark-file"]);
-    $this->input_media_file   = trim($hash["input-media-file"]);
-    $this->output_media_file  = trim($hash["output-media-file"]);
+    
+    $this->original_hash = $hash;
+    
+    // Set attributes from XML on object
+    // Creat job file objects from file info
+    foreach($hash as $hash_key => $hash_value) {
+      $method_name = "set_".str_replace("-", "_", $hash_key);
+      if(method_exists($this, $method_name)) {
+        $this->$method_name($hash_value);
+      } else {
+        $var_name = str_replace("-", "_", $hash_key);
+        $this->$var_name = trim($hash_value);
+      }
+    }
+  }
+  
+  function set_input_media_file($hash_value) {
+    $this->input = new FlixCloudJobInputFile($hash_value["url"], get_object_vars($hash_value));
+    $this->input_media_file = &$this->input->url; // Backward compatibility
+  }
+  
+  function set_output_media_file($hash_value) {
+    $this->output = new FlixCloudJobOutputFile($hash_value["url"], get_object_vars($hash_value));
+    $this->output_media_file = &$this->output->url; // Backward compatibility
+  }
+  
+  function set_watermark_media_file($hash_value) {
+    $this->watermark = new FlixCloudJobWatermarkFile($hash_value["url"], get_object_vars($hash_value));
   }
 }
 
---
+++ README.markdown
@@ -0,0 +1,148 @@
+FlixCloud API PHP Library
+==========================
+
+Author:  Steve Heffernan <steve@sevenwire.com>
+Version: 1.0.6
+Date:    04-01-2009
+
+For more details on the FlixCloud API requirements visit
+http://flixcloud.com/api/
+
+There are two main functions: Job Requests, and Notification Handling.
+
+
+
+TRANSCODING JOB REQUEST
+------------------------
+The FlixCloudJob object uses cURL to send an XML formatted job request to
+https://www.flixcloud.com/jobs. It then uses the SimpleXMLElement library
+to parse any returned XML.
+
+ **API key is required and can be found at https://flixcloud.com/settings**
+ **Recipe ID is required and can be found at http://flixcloud.com/overviews/recipes**
+
+
+### EXAMPLE 1
+
+    require("FlixCloud.php");
+
+    // First parameter is API key, second is Recipe ID
+    $fcj = new FlixCloudJob("2j1l:kd3add:0:ivzf:2e1y");
+    $fcj->recipe_id = 99;
+    $fcj->set_input("http://www.example.com/videos/input.mpg"); // Add username/password if needed
+    $fcj->set_output("ftp://www.example.com/httpdocs/videos/output.flv", array("user" => "username", "password" => "password");
+    $fcj->set_watermark("http://www.example.com/videos/watermark.png"); // Add username/password if needed
+
+    if($fcj->send()) {
+      // Job was successfully requested. Store job ID if needed.
+      echo "Success. Job ID is ".$fcj->id." Submitted at ".$fcj->initialized_job_at;
+    } else {
+      // Do something with the errors
+      foreach($fcj->errors as $error) {
+        echo $error;
+      }
+    }
+
+
+### EXAMPLE 2 (using params hash)
+
+    require("FlixCloud.php");
+
+    $fcj = new FlixCloudJob("2j1l:kd3add:0:ivzf:2e1y", array(
+      "recipe_id"       => 99,
+      "input_url"       => "http://www.example.com/videos/input.mpg",
+      // Add input_user, input_password if needed
+      "output_url"      => "sftp://www.example.com/httpdocs/videos/output.flv",
+      "output_user"     => "username",
+      "output_password" => "password",
+      "watermark_url"   => "http://www.example.com/videos/watermark.png",
+      // Add watermark_user, watermark_password if needed
+    ));
+
+    if($fcj->success) {
+      echo "Success. Job ID is ".$fcj->id." Submitted at ".$fcj->initialized_job_at;
+    } else {
+      foreach($fcj->errors as $error) {
+        echo $error;
+      }
+    }
+
+
+
+NOTIFICATION HANDLING
+----------------------
+The FlixCloudNotificationHandler class is used to capture and parse XML data sent from
+FlixCloud when a job has been completed.
+
+The possible states of the FlixCloudJobNotification object are "successful_job",
+"cancelled_job", or "failed_job". A callback function allows you to respond accordingly.
+
+**Notification URL must be set in https://flixcloud.com/settings**
+**A file following the example below should be at that URL**
+
+### EXAMPLE
+
+    require("FlixCloud.php");
+
+    $job_notification = FlixCloudNotificationHandler::catch_and_parse();
+
+    switch ($job_notification->state) {
+        case "successful_job":
+
+          echo $job_notification->id."\n";
+
+          echo $job_notification->output->url."\n";
+          echo $job_notification->output->cost."\n";
+
+          echo $job_notification->input->url."\n";
+          echo $job_notification->input->cost."\n";
+          // Other info available. See code.
+
+          break;
+        case "cancelled_job":
+          // Do something else
+          break;
+        case "failed_job":
+          echo $job_notification->error_message;
+          break;
+        default:
+    }
+
+
+
+Versions
+---------
+
+    Version 1.0.7 - 04/17/09    Updated notification object to match new XML version
+
+    Version 1.0.6 - 04/14/09    Cleaned up the documentation a little.
+
+    Version 1.0.5 - 04/06/09     Added options for pointing to the certificate or certificate directory.
+
+    Version 1.0.4 - 04/06/09    Added option to connect insecurely incase SSL cert isn't working.
+                                Like curl -k
+
+    Version 1.0.3 - 04/06/09    Added backward compatability for recipe as second argument
+                                Added Curl error checking
+                                Added more detailed unknown error reporting
+                                Formatted XML a little
+
+    Version 1.0.2 - 04/05/09    * Now requiring API key on object creation, and added an optional params hash
+                                  for setting file info and sending on creation (example 2). *
+                                * Recipe is no longer second argument *
+                                * Broke up notification handling into 2 objects (see example) *
+                                    FlixCloudNotificationHandler => Catch and parse XML
+                                    FlixCloudJobNotification => Holds job notification info.
+                                Added escaping of special characters when creating XML tags.
+                                Removed protocol checking for now, because may want to just let the API do that.
+                                On job->send changed status code handling to a switch statement.
+
+    Version 1.0.1 - 04/02/09    Fixed acceptable protocols for some of the file types.
+
+
+
+NOTES:
+-------
+Suggestions -> steve@sevenwire.com
+
+The windows version of curl will automatically look for a CA certs file named ´curl-ca-bundle.crt´, either in the same directory as curl.exe, or in the Current Working Directory, or in any folder along your PATH
